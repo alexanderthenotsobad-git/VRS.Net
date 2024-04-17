9d530791fd67de456f2fb1e0a2f1b1247b84d0db
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-04-17 13:15:07 -0400

    Token Implementation
    We were able to request token, download it and use it to get values. Also changed values returned to include user name.

--- VRSDataManager/Controllers/ValuesController.cs
+++ VRSDataManager/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace VRSDataManager.Controllers
 {
@@ -13,7 +14,8 @@ namespace VRSDataManager.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            return new string[] { "value1", "value2", userId };
         }
 
         // GET api/values/5
