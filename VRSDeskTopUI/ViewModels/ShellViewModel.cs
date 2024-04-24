d6551db1331c27116eeadac5d59fa5b7362af450
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-04-23 23:54:31 -0400

    Created MVVM Architecture
    We also inserted a few new files:
    ShellView.xaml
    Bootstrapper.cs

---
+++ VRSDeskTopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRSDesktopUI.ViewModels;
+
+namespace VRSDesktopUI.ViewModels
+{
+    internal class ShellViewModel
+    {
+    }
+}
