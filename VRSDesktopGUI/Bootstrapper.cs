dbde723bc46a160c49626ee2154625d7e5c1fb37
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-04-25 15:04:31 -0400

    Dependency Injection Start
    Started configuring the dependency injection by
    override Configure, Startup, GetInstance,
    GetAllInstances, BuildUp methods in Caliburn's
    "Simple Container"

--- VRSDesktopGUI/Bootstrapper.cs
+++ VRSDesktopGUI/Bootstrapper.cs
@@ -11,13 +11,36 @@ namespace VRSDesktopGUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
         }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<ShellViewModel>();
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
