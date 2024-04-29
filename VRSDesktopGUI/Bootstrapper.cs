6285d8beccb6a06afad650f052a6f7b23ca2c173
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-04-29 12:04:55 -0400

    Add WPF Project with MVVM
    Adds our WPF user interface project and configures it with Caliburn.Micro for our MVVM framework, and includes the Simple Container DI system.

--- VRSDesktopGUI/Bootstrapper.cs
+++ VRSDesktopGUI/Bootstrapper.cs
@@ -20,9 +20,17 @@ namespace VRSDesktopGUI
         protected override void Configure()
         {
             _container.Instance(_container);
+             
             _container
                 .Singleton<IWindowManager, WindowManager>()
                 .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
