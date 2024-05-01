ee3f66b391e5c7deb331958e9f44786ac2203c62
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-05-01 13:18:34 -0400

    This is a commit and sync from PC 4/1/24

---
+++ VRSDesktopGUI/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+<<<<<<< HEAD:VRSDeskTopUI/App.xaml.cs
+namespace VRSDesktopUI
+=======
+namespace VRSDesktopGUI
+>>>>>>> frontend:VRSDesktopGUI/App.xaml.cs
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
