465ac1797fccb2870c71f5c51a8c9c1b8eb09edc
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-05-05 15:41:49 -0400

    Began Development of DB
    Created a few basic tables for employees,
    tables, and orders.

---
+++ VRSData/dbo/tables/Order.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+	[OrderId] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[EmployeeID] NVARCHAR(128) NOT NULL,
+    [OrderDaateTime] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [TableID] NCHAR(10) NULL, 
+    [ItemID] NCHAR(10) NULL
+)
---
+++ VRSData/dbo/tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[EmployeeId] NCHAR(128) NOT NULL PRIMARY KEY,  
+    [FirstName] NCHAR(50) NOT NULL, 
+    [LastName] NCHAR(50) NOT NULL, 
+    [EmailAddress] NCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
