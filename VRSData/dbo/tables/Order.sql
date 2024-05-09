559a3566bd48d9679bb380b239ae7b81ee18bbe8
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-05-09 16:57:38 -0400

    Modified a field name on Order tbl

--- VRSData/dbo/tables/Order.sql
+++ VRSData/dbo/tables/Order.sql
@@ -2,7 +2,7 @@
 (
 	[OrderId] INT NOT NULL PRIMARY KEY IDENTITY, 
 	[EmployeeID] NVARCHAR(128) NOT NULL,
-    [OrderDaateTime] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [OrderDateTime] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [TableID] NCHAR(10) NULL, 
     [ItemID] NCHAR(10) NULL
 )
